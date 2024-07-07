d1610eaffb90b4198d74b69ab2127b08679051ab
Toluwalemi Oluwadare <toluwalemisrael@gmail.com>
2024-07-07 02:34:29 +0100

    Initial commit

--- backend/prestart.sh
+++ backend/prestart.sh
@@ -1,5 +1,9 @@
 #! /usr/bin/env bash
 
+echo "Running prestart.sh script..."
+echo "Current working directory:" && pwd
+echo "Listing contents of current directory:" && ls -la
+
 # Let the DB start
 python ./app/backend_pre_start.py
 
